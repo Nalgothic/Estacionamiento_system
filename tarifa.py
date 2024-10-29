8251eebffcc86ad836026e971c51dda2965b1f15
Ninna <160647521+Nalgothic@users.noreply.github.com>
2024-10-29 15:24:26 -0300

    Update tarifa.py

--- tarifa.py
+++ tarifa.py
@@ -5,6 +5,10 @@ import time
 def calculo_tarifa(hora_ingreso, hora_egreso):
     hora_ingreso=0
     hora_egreso=0
+
+#hacer que tome el evento del boton de inicio de timer para el vehiculo o cuando guarde el 
+    #registro, que se inicie el tiempo dentro del establecimiento
+    #que cada 15 min sume 500 a la tarifa. 
     
     
     
