36dbcd56206d81d9a478cb96745b264dcca1a8d2
Aldana <160647521+Nalgothic@users.noreply.github.com>
2024-10-30 15:07:51 -0300

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -24,6 +24,47 @@ def egreso_vehiculo(patente,hora, fecha, tarifa, medio_pago):
         print(e)
         return False
     
+    
+def listar_ingresos():
+    try:
+        conexion = Conexion()
+        cursor = conexion.conectar()
+        sql = """
+        SELECT n_operacion, patente, hora_ingreso, fecha_ingreso, cochera 
+        FROM vehiculos
+        WHERE tarifa>=0 
+        ORDER BY cochera
+        """
+        cursor.execute(sql)
+        autos = cursor.fetchall()
+        return True, autos
+    except Exception as e:
+        print(e)
+        return False, None
+    finally:
+        conexion.desconectar()
+        
+def listar_egresos():
+    try:
+        conexion = Conexion()
+        cursor = conexion.conectar()
+        sql = """
+        SELECT n_operacion, patente, hora_egreso, fecha_egreso, cochera, tarifa, medio_pago  
+        FROM vehiculos
+        WHERE tarifa<=1 
+        ORDER BY cochera
+        """
+        cursor.execute(sql)
+        autos = cursor.fetchall()
+        return True, autos
+    except Exception as e:
+        print(e)
+        return False, None
+    finally:
+        conexion.desconectar()
+        
+        
+    
 
         
         
